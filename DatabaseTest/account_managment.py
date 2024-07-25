4184f42731a8f280551eaef6366c199c77468232
CharlieAtki <164878103+CharlieAtki@users.noreply.github.com>
2024-07-25 14:59:04 +0100

    Account managment with SQLite
    Fully working code with error handling. The users data is now stored within a SQL database

--- DatabaseTest/account_managment.py
+++ DatabaseTest/account_managment.py
@@ -2,7 +2,43 @@ import sqlite3
 import tkinter as tk
 from tkinter import ttk, messagebox
 
-def data_integrity(username):
+
+def password_check(username: str, password: str) -> bool:
+    # pull the password from the database - check it matched the input password
+    # Connect to the SQLite database
+    conn = sqlite3.connect('erp_system.db')
+
+    # Create a cursor object to interact with the database
+    cursor = conn.cursor()
+
+    try:
+        # Checks that the password stored in the database matches the input password
+        cursor.execute("SELECT password FROM users WHERE username = ?", (username,))
+
+        # Fetches the next row of the result set. If there are no more rows, it returns None.
+        result = cursor.fetchone()
+
+        # if the row can be found
+        if result:
+            # assigning the password value
+            stored_password = (result[0])
+            # if the stored password and input password are equal
+            if stored_password == password:
+                return True
+            else:
+                messagebox.showerror("Error", "Password incorrect")
+                return False
+        else:
+            return False
+    except sqlite3.Error as e:
+        messagebox.showinfo("Error", f"An Error Occurred: {e}")
+        return False
+    finally:
+        conn.close()
+
+
+# This function
+def data_integrity(username: str) -> bool:
     # Connect to the SQLite database
     conn = sqlite3.connect('erp_system.db')
 
@@ -14,12 +50,11 @@ def data_integrity(username):
         cursor.execute("SELECT 1 FROM users WHERE username = ?", (username,))
 
         # checks whether the row matches my query conditions.
-        # If the username exists in the database a row will be returned
-        # This will be a boolean
+        # If the username exists in the database a row will return True
+        # If the name does not exist, False will be returned.
         exists: bool = cursor.fetchone() is not None
 
         if exists:
-            messagebox.showinfo("Username", "Username is already taken")
             return False
         else:
             return True
@@ -30,6 +65,7 @@ def data_integrity(username):
     # Once all conditions are met, close the database
     finally:
         conn.close()
+
 
 def on_account_creation(username_entry, password_entry):
     # All data used within the database
@@ -82,6 +118,24 @@ def on_account_creation(username_entry, password_entry):
         finally:
             # Ensure the connection is closed
             conn.close()
+    else:
+        messagebox.showinfo("Username", "Username is already taken")
+
+
+def on_login(username_entry, password_entry):
+    username = username_entry.get()
+    password = password_entry.get()
+
+    # If the username is taken
+    # and if Password check is True - pass matches the username, login
+    if not data_integrity(username) and password_check(username, password):
+        try:
+            # load the homepage
+            print("Logged in load home page")
+        except Exception as e:
+            messagebox.showinfo("Error", f"An Error Occurred: {e}")
+    elif data_integrity(username):
+        messagebox.showinfo("Error", "That username does not exist")
 
 
 window = tk.Tk()
@@ -94,11 +148,14 @@ password_label = ttk.Label(window, text='Password')
 password_entry = ttk.Entry(window, show='*')
 account_creation_button = ttk.Button(window, text='Create Account',
                                      command=lambda: on_account_creation(username_entry, password_entry))
+login_button = ttk.Button(window, text='LogIn',
+                          command=lambda: on_login(username_entry, password_entry))
 
 username_label.grid(column=0, row=0, padx=5, pady=5)
 username_entry.grid(column=1, row=0, padx=5, pady=5)
 password_label.grid(column=0, row=1, padx=5, pady=5)
 password_entry.grid(column=1, row=1, padx=5, pady=5)
 account_creation_button.grid(column=0, row=2, columnspan=2, padx=5, pady=2, sticky='ew')
+login_button.grid(column=0, row=3, columnspan=2, padx=5, pady=2, sticky='ew')
 
 window.mainloop()
