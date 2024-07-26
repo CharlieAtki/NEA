efce3ea3749ad7e6d341ed767cff1c5a5ee22fe0
CharlieAtki <164878103+CharlieAtki@users.noreply.github.com>
2024-07-26 20:28:50 +0100

    Test
    Was not letting me go onto the dev branch on my windows PC - this could be due to my Mac.

--- DevelopmentBranch/account_managment.py
+++ DevelopmentBranch/account_managment.py
@@ -22,9 +22,9 @@ def password_check(username: str, password: str) -> bool:
         # if the row can be found
         if result:
             # assigning the password value
-            stored_password_hash = (result[0])
+            stored_password = (result[0])
             # if the stored password and input password are equal
-            if bcrypt.checkpw(password.encode('utf-8'), stored_password_hash.encode('utf-8')):
+            if stored_password == password:
                 return True
             else:
                 messagebox.showerror("Error", "Password incorrect")
@@ -83,23 +83,24 @@ def on_account_creation(username_entry, password_entry):
             # Create a cursor object to interact with the database
             cursor = conn.cursor()
 
-            password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            # Hash the password
+            # Make sure to check how the program interacts with the database
+            password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
             # Create the users table if it doesn't exist
             cursor.execute('''
             CREATE TABLE IF NOT EXISTS users (
                 user_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username TEXT NOT NULL UNIQUE,
-                password_hash TEXT NOT NULL,
+                password_hash TEXT,
                 days INTEGER,
                 stock INTEGER,
                 reorder_level INTEGER               
             )
             ''')
 
-            cursor.execute(
-                "INSERT INTO users (username, password_hash, days, stock, reorder_level) VALUES (?, ?, ?, ?, ?)",
-                (username, password_hash, days, stock, reorder_level))
+            cursor.execute("INSERT INTO users (username, password_hash, days, stock, reorder_level) VALUES (?, ?, ?, ?, ?)",
+                           (username, password, days, stock, reorder_level))
 
             # Commit the changes
             conn.commit()
